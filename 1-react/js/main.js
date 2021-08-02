4c0ba30c29a4eb0e2c3291e7c197278bd7b57d11
Jaehyun Kim <jaehyun@liveanywhere.me>
2021-08-02 20:48:59 +0900

    state

--- 1-react/js/main.js
+++ 1-react/js/main.js
@@ -1,15 +1,43 @@
-const element = (
-  <>
-    <header>
-      <h2 className="container">검색</h2>
-    </header>
-    <div className="container">
-      <form>
-        <input type="text" placeholder="검색어를 입력하세요" autoFocus />
-        <button type="reset" className="btn-reset"></button>
-      </form>
-    </div>
-  </>
-);
+class App extends React.Component {
+  constructor() {
+    super();
 
-ReactDOM.render(element, document.querySelector("#app"));
+    this.state = {
+      searchKeyWord: "Hello",
+    };
+  }
+
+  handleChangeInput(event) {
+    // this.state.searchKeyWord = event.target.value;
+    // this.forceUpdate();
+
+    this.setState({
+      searchKeyWord: event.target.value,
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <header>
+          <h2 className="container">검색</h2>
+        </header>
+        <div className="container">
+          <form>
+            <input
+              type="text"
+              placeholder="검색어를 입력하세요"
+              autoFocus
+              value={this.state.searchKeyWord}
+              onChange={event => this.handleChangeInput(event)}
+            />
+            <button type="reset" className="btn-reset"></button>
+          </form>
+        </div>
+      </>
+    )
+  }
+
+}
+
+ReactDOM.render(<App/>, document.querySelector("#app"));
